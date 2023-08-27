d4e657baa66a9c65015d785bf57b3e4c08dbdd0f
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-08-27 12:20:02 -0300

    safe HTML jinja bugfix Paulo

--- controllers/blog.py
+++ controllers/blog.py
@@ -11,16 +11,13 @@ bp = Blueprint('blog', __name__, url_prefix='/')
 
 @bp.route("/", methods=['GET'])
 def index():
+
     chychoVault = get_database()
     posts = chychoVault["posts2"]
     post_details = posts.find()
-
-    print(type(post_details))
 
     posts = []
     for post in post_details:
         posts.append(post)
 
-    print(type(posts))
-
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=posts[:100:-1])
